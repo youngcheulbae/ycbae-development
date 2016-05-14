79720796960c1bffe35cbf240c196e2b768ef9fc
ycbae <youngcheul.bae@gmail.com>
2016-05-14 23:19:02 +0900

    commit springboot project

---
+++ springboot/src/main/java/com/ycbae/sbt/TestController.java
@@ -0,0 +1,14 @@
+package com.ycbae.sbt;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	@RequestMapping("/")
+	public String root() {
+		return "Spring Boot Test";
+	}
+
+}
---
+++ springboot/src/test/java/com/ycbae/sbt/SpringbootApplicationTests.java
@@ -0,0 +1,18 @@
+package com.ycbae.sbt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringbootApplication.class)
+@WebAppConfiguration
+public class SpringbootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
